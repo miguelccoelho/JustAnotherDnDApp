a0241338790796dd49a8593501fda8c09ea14f4d
MiguelCCoelho <miguelccoelho@gmail.com>
2017-03-31 11:44:58 +0100

    Corrections on folders

---
+++ Just Another DnD App/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Just_Another_DnD_App
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
